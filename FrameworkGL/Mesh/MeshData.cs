38469d225ad7c37d2354a249fde37d00c59c6263
Khin Baptista <kerbaptista@inf.ufrgs.br>
2014-11-21 13:04:30 +0000

    Mesh class added
    Vertex unit and mesh data finalized;
    Mesh added.

---
+++ FrameworkGL/Mesh/MeshData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameworkGL
+{
+    class MeshData
+    {
+        public List<VertexUnit> vertices { get; private set; }
+        public List<uint> indices { get; private set; }
+
+        public MeshData() {
+            vertices = new List<VertexUnit>();
+            indices = new List<uint>();
+        }
+
+        public void AddVertexUnit(VertexUnit unit) {
+            bool add = true;
+            int i = 0;
+
+            while (add == true && i < vertices.Count) {
+                if (vertices[i] == unit) {
+                    add = false;
+                    indices.Add((uint)i);
+                }
+
+                i++;
+            }
+
+            if (add) {
+                vertices.Add(unit);
+                indices.Add((uint)vertices.Count - 1);
+            }
+        }
+    }
+}
---
+++ FrameworkGL/Mesh/VertexUnit.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics;
+
+namespace FrameworkGL
+{
+    class VertexUnit
+    {
+        public Vector3 position { get; private set; }
+        public Vector3 normal { get; private set; }
+        public Vector2 texCoord { get; private set; }
+
+        public VertexUnit(Vector3 position, Vector3 normal, Vector2 texCoord) {
+            this.position = position;
+            this.normal = normal;
+            this.texCoord = texCoord;
+        }
+
+        public static bool operator ==(VertexUnit a, VertexUnit b) {
+            return
+                a.position == b.position &&
+                a.normal == b.normal &&
+                a.texCoord == b.texCoord;
+        }
+
+        public static bool operator !=(VertexUnit a, VertexUnit b) {
+            return !(a == b);
+        }
+
+        public static bool PositionEquals(VertexUnit a, VertexUnit b) {
+            return a.position == b.position;
+        }
+
+        public static bool NormalEquals(VertexUnit a, VertexUnit b) {
+            return a.normal == b.normal;
+        }
+
+        public static bool TexCoordEquals(VertexUnit a, VertexUnit b) {
+            return a.texCoord == b.texCoord;
+        }
+
+        public override bool Equals(object obj) {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode() {
+            return base.GetHashCode();
+        }
+    }
+}
